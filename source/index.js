db447635d51ea5d1408540d17f3e3096f0e62f8f
Jerd <inquisitivequin@gmail.com>
2017-07-04 16:58:10 -0700

    poat

--- source/index.js
+++ source/index.js
@@ -1,6 +1,24 @@
-import {keyVal, test} from '../xtrnl/xrnl';
+class Person  {
+
+	constructor(name) {
+		this.name = name;
+	}
+
+	greet() {console.log(this.name + ' and I am ' + this.age + ' fool')}
+}
 
 
-console.log(keyVal)
+let poat = new Person('Poat');
 
-test()
+poat.greet()
+
+class Pwt extends Person {
+	constructor(age) {
+		super('Pwt');
+		this.age = age
+	}
+}
+
+let pet = new Pwt(42)
+
+pet.greet()
