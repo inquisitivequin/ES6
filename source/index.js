4bbc68f884f68fc0139406a6160f74c19d9cc5f0
Jerd <inquisitivequin@gmail.com>
2017-07-07 11:08:08 -0700

    iter-yay

--- source/index.js
+++ source/index.js
@@ -1,11 +1,11 @@
-let num = [1,2,3,4,5]
+let arr = [1,2,3,4]
 
-num[Symbol.toPrimitive] = function() {
-	let sum = 0;
-	for (var i = num.length - 1; i >= 0; i--) {
-		sum = num[i] + sum
-	}
-	return sum
-}
+let it = arr[Symbol.iterator]()
 
-console.log(num + 1)
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next() != true)
+console.log(it.next().done != true)
+console.log(arr)
+
