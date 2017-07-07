69f2b46cf2eca68336bdeb7a07ac4b480a0435ba
Jerd <inquisitivequin@gmail.com>
2017-07-06 20:29:30 -0700

    fin sec-4

--- source/index.js
+++ source/index.js
@@ -1,10 +1,11 @@
-let sym = Symbol('dabug')
+let num = [1,2,3,4,5]
 
-let altsym = Symbol('dabog')
-
-let obj = {
-	name: 'poat',
-	[sym]: 22
+num[Symbol.toPrimitive] = function() {
+	let sum = 0;
+	for (var i = num.length - 1; i >= 0; i--) {
+		sum = num[i] + sum
+	}
+	return sum
 }
 
-console.log(obj[sym])
+console.log(num + 1)
