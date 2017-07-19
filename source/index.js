c6fb75d60e897b53b62084b3b6c15024bfbce500
Jerd <inquisitivequin@gmail.com>
2017-07-18 22:24:23 -0700

    wowoxy

--- source/index.js
+++ source/index.js
@@ -1,26 +1,15 @@
-let pers = {
-	name: 'poat'
+function log(msg) {
+	console.log('Log fool ' + msg)
 }
 
 let han = {
-
-	}
-
-let phan = {
-	get: function(tar, na) {
-		return na in tar ? tar[na] : 0
-	},
-	set: function(target, prop, val) {
-		if(val.length >= 2) {
-			Reflect.set(target, prop, val)
+	apply: function(tar, thisAr, argList) {
+		if (argList.length == 1) {
+			return Reflect.apply(tar, thisAr, argList)
 		}
 	}
 }
 
-var prox = new Proxy({}, han);
+let prox = new Proxy(log, han)
 
-let pprox = new Proxy(prox, phan)
-
-Reflect.setPrototypeOf(pers, pprox)
-
-console.log(pers.hobs)
+prox('hola')
