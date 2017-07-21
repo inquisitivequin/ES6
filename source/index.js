6b28636645f58f8707b933eabfca485e5653972b
Jerd <inquisitivequin@gmail.com>
2017-07-20 22:35:42 -0700

    btn

--- source/index.js
+++ source/index.js
@@ -1,2 +1,8 @@
-import './style.css'
+import './style.css';
+import * as ELS from "./elms";
 
+ELS.EL_SRCH_BTN.addEventListener('click', searchWeath)
+
+function searchWeath() {
+	alert('Potato')
+}
