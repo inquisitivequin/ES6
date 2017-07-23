bf6a11d5b64d241c269998cc8271f3eaffb13b2d
Jerd <inquisitivequin@gmail.com>
2017-07-23 10:10:52 -0700

    fini

--- source/index.js
+++ source/index.js
@@ -4,7 +4,7 @@ import {Http} from '../external/http';
 import {WeatherData, WEATHER_PROXY_HANDLER} from '../external/weather-data'
 
 
-const apId = '0bd48a326627df68607a14e013e4384b';
+const apId = '';
 
 ELS.EL_SRCH_BTN.addEventListener('click', searchWeath)
 
@@ -13,21 +13,27 @@ function searchWeath() {
 	if (CITY_NAME.length == 0) {
 		return alert('Enter a city name fool!')
 	}
-	const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME + '&units=metric&appid=apId'
+	ELS.EL_LOADING.style.display = 'block'
+	ELS.EL_WEATHER.style.display = 'none'
+	const URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + CITY_NAME 
+					+ '&units=metric&appid=0bd48a326627df68607a14e013e4384b'
 	Http.fetchData(URL)
 		.then(responseData => {
-			const WEATHER_DATA = new WeatherData(CITY_NAME, responseData.weather[0].description.toUpperCase())
+			const WEATHER_DATA = new WeatherData(CITY_NAME, responseData.weather[0]
+				.description.toUpperCase())
 			const WEATHER_PROXY = new Proxy(WEATHER_DATA, WEATHER_PROXY_HANDLER)
-			WEATHER_PROXY.temperature = responseData.main.temp
+			WEATHER_PROXY.temp = responseData.main.temp
 			updateWeather(WEATHER_PROXY)
 		})
 		.catch(error => alert(error))
 }
 
 function updateWeather(weatherData) {
-	ELS.EL_WEAT_CITY.textContent = weatherData.cityName
-	ELS.EL_WEAT_TEMP.textContent = weatherData.temp
-	ELS.EL_WEAT_DESC.textContent = weatherData.desc
+	console.log(weatherData.temp)
+	ELS.EL_WEATH_CITY.textContent = weatherData.cityName
+	ELS.EL_WEATH_TEMP.textContent = weatherData.temp
+	ELS.EL_WEATH_DESC.textContent = weatherData.desc
 
+	ELS.EL_LOADING.style.display = 'none'
 	ELS.EL_WEATHER.style.display = 'block'
 }
